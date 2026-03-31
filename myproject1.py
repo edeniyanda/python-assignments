6c28f16dde5f7ccc29435dcd604336fd87b77551
Eden Iyanda <106480499+edeniyanda@users.noreply.github.com>
2026-03-31 19:51:50 +0100

    Clean up print statements in myproject1.py
    Removed unnecessary print statements to streamline output.

--- myproject1.py
+++ myproject1.py
@@ -1,8 +1,6 @@
 print("Hello, World!")
 
-print("Welcome to Python programming.")
-print("This is a simple script to demonstrate basic syntax.")
-print("Let's do some basic arithmetic:")
+
 a = 5
 b = 3
 print("The sum of a and b is:", a + b)
@@ -10,4 +8,7 @@ print("The difference of a and b is:", a - b)
 print("The product of a and b is:", a * b)
 print("The quotient of a and b is:", a / b)
 print("That's all for now. Happy coding!")  
+
+
+
 
